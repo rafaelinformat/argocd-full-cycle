d62706086dc1074cc7a4cfbeb9c0c923ef6000e1
= <=>
2023-03-09 18:03:41 -0300

    fixed gitlab workflow

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ import "net/http"
 
 func main()  {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		w.Write([]byte("Hello word in Go lang"))
+		w.Write([]byte("<h1>Hello word in Go lang  with Docker and Kubernetes</h1>"))
 	})
 	http.ListenAndServe(":8080", nil )
 	
